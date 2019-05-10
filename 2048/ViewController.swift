51fa592279ef537e9384a939d24267913f9718e3
Alexander Andreev <alexander.andreev@it-olimp-tomsk.com>
2019-05-10 18:07:42 +0700

    create single view app

---
+++ 2048/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  2048
+//
+//  Created by Александр Андреев on 10/05/2019.
+//  Copyright © 2019 Александр Андреев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
